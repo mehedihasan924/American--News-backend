bdee5299b9e4321ec8cfc8368ec0fec037c571ae
Mehedi-Hasan <mdhasanmehedi924@gmail.com>
2023-12-01 01:17:52 -0800

    Some added

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 const cors = require('cors')
 const category=require('./Data/categories.json')
 const news=require("./Data/news.json")
-
 
 
 app.use(cors());
@@ -35,11 +34,6 @@ app.get('/', (req, res) => {
     }
 
   })
-
-
-
-
-
 
   app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
